1c00d2df6eb76f75b99701af7b7ae8e6a5baf57c
defigods <defigodss@gmail.com>
2020-12-10 16:42:07 -0600

    notes on execution

--- src/components/HistoryPastTrades/index.tsx
+++ src/components/HistoryPastTrades/index.tsx
@@ -10,6 +10,15 @@ const Wrapper = styled.div`
 `;
 
 
+//TODO: Receive all data of past Trades
+// This could be a web3 call parsing through LogTakes[2] = seller and
+// LogTakes[6] = Buyer and the trade data is there....
+//  This lends me to think that the user should incur web3 calls to check their
+//  price history... TODO: Need to throw a loading while loading in the history
+//TODO: Display that data into the scrolling chart depicted in mockup
+
+//data passed into component should be token, BUY/Sell, Size, Tx hash
+
 export default function () {
   return (
     <Wrapper>
