a9692be5d439aac91d1371e060cbd303f3100bb7
defigods <5880388+defigods@users.noreply.github.com>
2021-02-26 16:20:57 -0600

    added Rubicon logo

--- src/containers/Ramp/index.tsx
+++ src/containers/Ramp/index.tsx
@@ -3,20 +3,23 @@ import TokenList from '../../components/TokenList';
 import HistoryPastTrades from '../../components/HistoryPastTrades';
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk';
 import styled from 'styled-components';
+import { useActiveWeb3React } from '../../hooks';
 
-const Ramp = styled.div``;
 // https://docs.ramp.network/sdk-reference
 
 export default function () {
+  const { account, chainId } = useActiveWeb3React();
+
   new RampInstantSDK({
     hostAppName: 'Rubicon',
-    hostLogoUrl: 'https://yourdapp.com/yourlogo.png',
-    swapAmount: '150000000000000000000', // 150 ETH in wei
+    hostLogoUrl:
+      'https://user-images.githubusercontent.com/32072172/109361232-34969c80-784e-11eb-9b4d-91c09806eec1.png',
+    swapAmount: '100000000000000000', // .1 ETH in wei
     swapAsset: 'ETH',
-    userAddress: 'user blockchain address',
+    userAddress: String(account),
   })
     .on('*', (event) => console.log(event))
     .show();
 
-  return <>{/* <Ramp /> */}</>;
+  return <></>;
 }
